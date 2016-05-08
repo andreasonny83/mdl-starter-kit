4065767435b91d8f785a5dd1bda38e0fb93cfb60
Andrea Sonny <andreasonny83@gmail.com>
2016-05-08 18:22:29 +0100

    first version containing gulp task for generating a developpment version

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # mdl-starter-kit
+
+## License
+
+MIT © [Andrea Sonny](https://andreasonny.mit-license.org/@2016)
+
+
+## Third-Party Licenses
+
+[Third-Party Licenses](https://github.com/andreasonny83/mdl-starter-kit/blob/master/LICENSE-3RD-PARTY.md)
---
+++ gulp/gulp.config.js
@@ -0,0 +1,56 @@
+import path from 'path';
+
+const src = path.join(__dirname, '../app');
+const dist = path.join(__dirname, '../dist');
+const temp = path.join(__dirname, '../.tmp');
+
+const AUTOPREFIXER = [
+  'last 2 versions',
+  'safari >= 7',
+  'ie >= 9',
+  'ff >= 30',
+  'ios 6',
+  'android 4'
+];
+
+const config = {
+  src: src,
+  dist: dist,
+  temp: {
+    base: temp,
+    libs: path.join(temp, 'libs'),
+    sass: path.join(temp, 'styles'),
+    js: path.join(temp, 'scripts'),
+    styles: [
+      path.join(temp, 'libs', '*.css'),
+      path.join(temp, 'styles', '*.css')
+    ],
+    scripts: [
+      path.join(temp, 'libs', '*.js'),
+      path.join(temp, 'scripts', '*.js')
+    ]
+  },
+  bowers: path.join(src, 'bower_components'),
+  autoprefixer: AUTOPREFIXER,
+  files: [
+    path.join(src, '*.html')
+  ],
+  styles: {
+    src: path.join(src, 'sass/**/*.scss'),
+    dest: path.join(dist, 'styles')
+  },
+  scripts: {
+    src: path.join(src, 'scripts/entry.js'),
+    webpack: path.join(__dirname, '../webpack.config.js'),
+    dest: path.join(dist, 'scripts')
+  }
+};
+
+var env = {
+  env: 'DEV'
+};
+
+module.exports = {
+  config: config,
+  env: env
+};
